007460108bf11e0fa3e686591ee8c042d9226f0b
Administrator <Administrator@USER-20191226KJ>
2020-05-21 17:18:05 +0800

    第三次提交

--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("测试分享项目到本地仓库");
         System.out.println("2222");
+        System.out.println("3333");
+        
 	}
 }
